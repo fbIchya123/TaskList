bf938b96ac12934c5bddc0b798bfb9cb58d850c6
Eugeniy <n0n1est1deus@gmail.ru>
2023-07-03 14:46:25 +0400

    auth

--- script_auth.php
+++ script_auth.php
@@ -1,12 +1,11 @@
 <?php
-//ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 session_start();
 include 'connect_db.php';
 
 $login = $_POST['login'];
-$password = $_POST['password'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 $created_at = date("Y-m-d");
-$redirect = "main.php";
 
 
 //Запрос на проверку наличия аккаунта
@@ -16,13 +15,14 @@ $query->execute([':name' => $login, ':password' => $password]);
 
 //Проверка наличия акка в базе
 if ($row = $query->fetch(PDO::FETCH_LAZY)){
-    $_SESSION['user_id'] = $row->id;    
-    header("Location: " . $redirect);
+    $_SESSION['user_id'] = $row->id;
     
 } else {
     //Добавление акка, если его нет
     $query = $pdo->prepare("INSERT INTO `users` (name, password, created_at) VALUES (:name, :password, :created_at)"); 
     $query->execute([':name' => $login, ':password' => $password, ':created_at' => $created_at]);
-    header("Location: " . $redirect);
+    $_SESSION['user_id'] = $row->id;
+    //header("Location: main.php");
 }
 
+password_verify($password)
